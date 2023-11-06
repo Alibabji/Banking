cdb0f714223afdfa3495bf11e89d2e5a3a9b6efe
alibabji <woodzin070929@gmail.com>
2023-11-06 18:37:12 -0500

    Completed step 1 basic banking system

--- banking/소스.cpp
+++ banking/소스.cpp
@@ -2,41 +2,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int id, amount,order=0;
+struct acc
+{
+	string name;
+	int balance;
+};
+acc num[1000];
+int accnum[1000];
+
 void create()
 {
-	Bank 
-	cout << "[계좌계설]" << endl<<"계좌ID: ";
-
+	cout << "[계좌계설]\n계좌ID: ";
+	cin >> id;
+	cout << "이름: "; cin >> num[id].name;
+	cout << "입금액: "; cin >> num[id].balance;
+	accnum[order] = id;
+	order++;
 }
+
 void deposit()
 {
-
+	cout << "[입  금]\n계좌ID: "; cin >> id;
+	cout << "입금액: "; cin >> amount;
+	num[id].balance += amount;
+	printf("입금완료\n");
 }
+
+//num=int(intput("input a number: "))
+
 void withdrawal()
 {
-
+	cout << "[출  금]\n계좌ID: "; cin >> id;
+	cout << "출금액: "; cin >> amount;
+	if (num[id].balance >= amount)
+	{
+		num[id].balance -= amount;
+		cout << "출금완료\n";
+	}
+	else
+	{
+		cout << "ERROR!! 출금액이 잔액보다 많습니다!!\n";
+	}
 }
+
 void ShowAllInfo()
 {
-
+	for (int j = 0; order>j; j++)
+	{
+		cout << "계좌ID: " << accnum[j] << "\n이름: " << num[accnum[j]].name << "\n잔액: " << num[accnum[j]].balance << endl<<endl;
+	}
 }
+
 int main(void)
 {
-	cout << "-----Menu-----" << endl << "1. 개좌계설" << endl << "2. 입금" << endl << "3. 출금" << endl << "4. 계좌번호 전체 출력" << endl << "5. 프로그램 종료" << endl << "선택: ";
-	int num; cin >> num;
-	switch (num)
+	int num;
+	while (true)
 	{
-	case 1:
-		create();
-	case 2:
-		deposit();
-	case 3:
-		withdrawal();
-	case 4:
-		ShowAllInfo();
-	case 5:
-		return 0;
+		cout << "-----Menu-----\n1. 개좌계설\n2. 입금\n3. 출금\n4. 계좌번호 전체 출력\n5. 프로그램 종료\n선택: ";
+		cin >> num;
+		switch (num)
+		{
+		case 1:
+			create();
+			break;
+		case 2:
+			deposit();
+			break;
+		case 3:
+			withdrawal();
+			break;
+		case 4:
+			ShowAllInfo();
+			break;
+		case 5:
+			return 0;
+		}
+		printf("\n");
 	}
-
 	return 0;
 }
