8e3f4436e6167c2188b900620bb86ce6d5b8ac36
alibabji <woodzin070929@gmail.com>
2023-12-11 18:47:37 -0500

    Step 5 complete

--- banking/main.cpp
+++ banking/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void PETC(); //Press Enter To Continue
 
 int id, amount,order=0;
+
 class Account
 {
 private:
@@ -14,6 +15,7 @@ private:
 public:
 	Account(int n) :balance(0), ID(n)
 	{	
+		
 		cout << "이름: "; 
 		name = new string;
 		cin >> *name;
@@ -56,60 +58,100 @@ public:
 	}
 };
 
-Account* acc[1000]; //creae 1000 account address
-int acclist[1000];
-
-void ShowAllInfo()
+class AccountHandler
 {
-	for (int i = 0; order>i; i++)
+private:
+	Account* accList[1000];
+	int accNum;
+	vector<int> accID;
+public:
+	AccountHandler() :accNum(0)
 	{
-		acc[acclist[i]]->showinfo();
+		for (int i = 0; i < 1000; i++)
+			accList[i] = NULL;
+		accID = {};
+	};
+	
+	void ShowMenu() const
+	{
+		cout << "-----Menu-----\n1. 개좌계설\n2. 입금\n3. 출금\n4. 계좌번호 전체 출력\n5. 프로그램 종료\n선택: ";
 	}
-}
+
+	void AddAccount()
+	{
+		cout << "[계좌계설]\n계좌ID: ";
+		cin >> id;
+		accID.push_back(id);
+		accList[accNum++] = new Account(id);
+	}
+
+	void ShowAllInfo() const
+	{
+		for (int i = 0; i < accNum; i++)
+			accList[i]->showinfo();
+	}
+
+	void Deposit()
+	{
+		vector<int>::iterator it;
+		cout << "[입  금]\n계좌ID: "; cin >> id;
+		cout << "입금액: "; cin >> amount;
+		it = find(accID.begin(), accID.end(), id);
+		if (it != accID.end())
+			accList[it - accID.begin()]->deposit(amount);
+		else
+		{
+			printf("!!존재하지 않는 계정입니다!!\n");
+			return;
+		}
+		printf("입금완료\n");
+	}
+
+	void Withdrawal()
+	{
+		vector<int>::iterator it;
+		cout << "[출  금]\n계좌ID: "; cin >> id;
+		cout << "출금액: "; cin >> amount;
+		it = find(accID.begin(), accID.end(), id);
+		if (it != accID.end())
+			accList[it - accID.begin()]->withdrawal(amount);
+		else
+		{
+			printf("!!존재하지 않는 계정입니다!!\n");
+			return;
+		}
+	}
+
+	~AccountHandler()
+	{
+		for (int i = 0; i < accNum; i++)
+			delete accList[i];
+	}
+};
 
 int main(void)
 {
-	fill_n(acclist, 1000, -1);
-	for (int i = 0; i < 1000; i++)
-		acc[i] = NULL;
-
+	AccountHandler handler;
 	int num;
 	while (true)
 	{
-		cout << "-----Menu-----\n1. 개좌계설\n2. 입금\n3. 출금\n4. 계좌번호 전체 출력\n5. 프로그램 종료\n선택: ";
+		handler.ShowMenu();
 		cin >> num;
 		switch (num)
 		{
 		case 1:
-			cout << "[계좌계설]\n계좌ID: ";
-			cin >> id;
-			acc[id] = new Account(id);
-			acclist[order] = id; order++;
+			handler.AddAccount();
 			break;
 		case 2:
-			cout << "[입  금]\n계좌ID: "; cin >> id;
-			cout << "입금액: "; cin >> amount;
-			if (acc[id] == NULL)
-			{
-				printf("!!존재하지 않는 계정입니다!!\n");
-				break;
-			}
-			acc[id]->deposit(amount);
-			printf("입금완료\n");
+			handler.Deposit();
 			break;
 		case 3:
-			cout << "[출  금]\n계좌ID: "; cin >> id;
-			cout << "출금액: "; cin >> amount;
-			acc[id]->withdrawal(amount);
+			handler.Withdrawal();
 			break;
 		case 4:
-			ShowAllInfo();
+			handler.ShowAllInfo();
 			break;
 		case 5:
-			for (int i = 0; i < 1||acclist[i]!=-1; i++)
-			{
-				delete acc[acclist[i]];
-			}
 			return 0;
 		}
 		PETC();
