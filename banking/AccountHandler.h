b42b9944de2458cb51754381699aa07a3165b16f
alibabji <woodzin070929@gmail.com>
2024-01-08 18:51:51 -0500

    Step 10 Complete

--- banking/AccountHandler.h
+++ banking/AccountHandler.h
@@ -3,12 +3,12 @@
 #define __ACCOUNTHANDLER_H__
 #include <vector>
 #include "Account.h"
-#include "BoundCheckPointArray.h"
+#include "BoundCheckArray.h"
 
 class AccountHandler
 {
 private:
-	BoundCheckPointArray accList;
+	BoundCheckArray<ACC_PTR> accList;
 	//Account* accList[1000];
 	int accNum, id, amount;
 	vector<int> accID;
---
+++ banking/BoundCheckArray.h
@@ -0,0 +1,65 @@
+#pragma once
+#ifndef __BOUND_CHECK_ARRAY_H__
+#define __BOUND_CHECK_ARRAY_H__
+#include "Account.h"
+#include "BankingCommonDecl.h"
+#include <bits/stdc++.h>
+using namespace std;
+typedef Account* ACC_PTR;
+
+template <typename T>
+class BoundCheckArray
+{
+private:
+	T* arr;
+	int arrlen;
+	BoundCheckArray(const BoundCheckArray& arr) { };
+public:
+	BoundCheckArray(int len = MAX_ACC);
+	T& operator[](int idx);
+	T& operator[](int idx) const;
+	int GetArrLen() const;
+	~BoundCheckArray();
+};
+
+template <typename T>
+BoundCheckArray<T>::BoundCheckArray(int len):arrlen(len)
+{
+	arr = new T[len]; 
+}
+
+template <typename T>
+T& BoundCheckArray<T>::operator[](int idx)
+{
+	if (idx < 0 || idx >= arrlen)
+	{
+		cout << "Array index out of bound exception" << endl;
+		exit(1);
+	}
+	return arr[idx];
+}
+
+template <typename T>
+T& BoundCheckArray<T>::operator[](int idx) const
+{
+	if (idx < 0 || idx >= arrlen)
+	{
+		cout << "Array index out of bound exception" << endl;
+		exit(1);
+	}
+	return arr[idx];
+}
+
+template <typename T>
+int BoundCheckArray<T>::GetArrLen() const
+{
+	return arrlen;
+}
+
+template <typename T>
+BoundCheckArray<T>::~BoundCheckArray()
+{
+	delete[]arr;
+}
+
+#endif
